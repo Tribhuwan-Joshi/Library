f5564406537b7e7aebfe21309542c89ce6bde412
Tribhuwan <mathincosmos@gmail.com>
2022-06-09 17:23:54 +0530

    add popup window for adding new book

---
+++ script.js
@@ -0,0 +1,8 @@
+const addBook = document.querySelector('#add-book');
+const addBookForm = document.querySelector('.add-book-form');
+var Done = true;
+addBook.addEventListener('click', () => {
+    addBookForm.style.visibility = "hidden";
+
+    
+})
