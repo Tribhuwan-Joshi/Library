c6599c1668863c851e18b4fa141d1556001efba6
Tribhuwan <mathincosmos@gmail.com>
2022-06-16 13:11:29 +0530

    add feature to toggle book status

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// Info Objects
-const totalCount = document.querySelector("#total_books");
-const readCount = document.querySelector("#total_read");
-const unreadCount = document.querySelector("#total_unread");
 const form = document.querySelector("form");
 const table = document.querySelector("table").getElementsByTagName("tbody")[0];
 table.textContent = "";
 
 // Book constructor
-function Book(bookId, title, author, pages, status) {
-  this.bookId = bookId;
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.status = status;
 }
+
+// new book form
+const title = document.querySelector("form input#title");
+const author = document.querySelector("form input#author");
+const pages = document.querySelector("form input#pages");
+const bookStatus = document.querySelector('input[name="status"]');
+
+// Book array
+let myLibrary = [];
 
 // hide new book form when clicked outside
 document.addEventListener("click", () => {
@@ -33,72 +37,117 @@ let bookFormVisible = false;
 const addBook = document.querySelector("button#add-book");
 addBook.addEventListener("click", (e) => {
   e.stopPropagation();
+
   if (bookFormVisible) return;
 
   addBookForm.style.visibility = "visible";
   bookFormVisible = true;
+  title.focus();
 });
-
-
 
 // delete book eventListener
 
 const deleteAll = document.querySelector("button#delete-all");
-deleteAll.addEventListener("click", (e)=>{
+deleteAll.addEventListener("click", (e) => {
   e.stopPropagation();
- 
+
   if (table.textContent != "") {
-    let response = confirm("Are you sure to delete all books?")
+    let response = confirm("Are you sure to delete all books?");
     if (response) {
       table.textContent = "";
-    
+      myLibrary = [];
+      resetInfoData();
     }
   }
-  else {
-    alert("No books to delete");
+});
+
+// renderBook function to render the book Object
+
+function renderBook() {
+  table.textContent = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let htmlContent = `<tr>
+                        <td>${i + 1}</td>
+                        <td>${myLibrary[i].title}</td>
+                        <td>${myLibrary[i].author}</td>
+                        <td>${myLibrary[i].pages}</td>
+                        <td class="status ${
+                          myLibrary[i].status
+                        }" data-Id=${i}>${myLibrary[i].status}</td>
+                        <td id="trash" title="remove this book" data-Id=${i}><img src="./icons/trash.png" alt=""></td>
+                    </tr>`;
+    let newRow = table.insertRow();
+    newRow.innerHTML = htmlContent;
   }
-})
-
-
-
-
-
+}
 
 // Book counters
 let totalBooks = 0;
 let readBooks = 0;
 let unreadBooks = 0;
 
-// new book form
-const title = document.querySelector("form input#title");
-const author = document.querySelector("form input#author");
-const pages = document.querySelector("form input#pages");
-const bookStatus = document.querySelector('input[name="status"]');
+// update info data
+const totalContainer = document.querySelector("#total_books");
+const readContainer = document.querySelector("#total_read");
+const unreadContainer = document.querySelector("#total_unread");
 
-// renderBook function to render the book Object
-
-function renderBook(book) {
-  let htmlContent = `<tr>
-                        <td>${book.bookId}</td>
-                        <td>${book.title}</td>
-                        <td>${book.author}</td>
-                        <td>${book.pages}</td>
-                        <td class="status ${book.status}">${book.status}</td>
-                        <td id="trash" title="remove this book"><img src="./icons/trash.png" alt=""></td>
-                    </tr>`;
-  let newRow = table.insertRow();
-  newRow.innerHTML = htmlContent;
+function updateInfo() {
+  totalContainer.textContent = totalBooks;
+  readContainer.textContent = readBooks;
+  unreadContainer.textContent = totalBooks - readBooks;
 }
 
-// Book array
-let myLibrary = [];
+//reset info data
+function resetInfoData() {
+  totalContainer.textContent = 0;
+  readContainer.textContent = 0;
+  unreadContainer.textContent = 0;
+
+  totalBooks = readBooks = unreadBooks = 0;
+}
+
+function addBookToLibrary(myBook) {
+  myLibrary.push(myBook);
+  renderBook();
+  form.reset();
+  addBookForm.style.visibility = "hidden";
+  bookFormVisible = false;
+}
+
 
 // create new book object when clicked
 const done = document.querySelector(".done button");
 const ele = document.getElementsByName("status");
-let bookId = 1;
+let statusContainer;
+
+
+// toggle status
+
+function toggleStatus() {
+  statusContainer.forEach((sc) => {
+    sc.addEventListener('click', (e) => {
+      let index = e.target.getAttribute('data-Id');
+      let textContent = e.target.textContent;
+      if (textContent == "Read") {
+        e.target.textContent = "Unread";
+        e.target.style.backgroundColor = "rgb(253, 155, 70)";
+        myLibrary[index].status = "Unread";
+        readBooks--;
+
+      }
+      else {
+        e.target.textContent = "Read";
+        e.target.style.backgroundColor = "rgb(125, 243, 131)";
+        myLibrary[index].status = "Read";
+        readBooks++;
+      }
+      updateInfo();
+    })
+  })
+}
+
+
 done.addEventListener("click", () => {
-  console.log(myLibrary);
   let statusValue = "";
   for (i = 0; i < ele.length; i++) {
     if (ele[i].checked) {
@@ -112,26 +161,31 @@ done.addEventListener("click", () => {
     pages.value == "" ||
     statusValue == ""
   ) {
+    alert("Please enter all the details ! ");
+    title.focus();
+
     return;
   } else {
-    let myBook = new Book(
-      bookId,
-      title.value,
-      author.value,
-      pages.value,
-      statusValue
-    );
+    if (statusValue == "Read") {
+      readBooks++;
+    } else {
+      unreadBooks++;
+    }
+
+    totalBooks++;
+
+    // update info data
+    updateInfo();
+    
+    let myBook = new Book(title.value, author.value, pages.value, statusValue);
     addBookToLibrary(myBook);
+
+    statusContainer = document.querySelectorAll('table .status');
+
+    toggleStatus();
+  
   }
-  // reset the form and hide it
 });
 
-function addBookToLibrary(myBook) {
-  myLibrary.push(myBook);
-  console.log(myLibrary);
 
-  renderBook(myBook);
-  addBookForm.style.visibility = "hidden";
-  bookFormVisible = false;
-  form.reset();
-}
+
